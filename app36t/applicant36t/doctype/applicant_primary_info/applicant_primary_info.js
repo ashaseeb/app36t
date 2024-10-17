4d0342a76b52c1ee3389f8c28341d995bf08cb63
Haseeb <ashaseeb@gmail.com>
2024-10-17 18:09:22 -0500

    First Commit

---
+++ app36t/applicant36t/doctype/applicant_primary_info/applicant_primary_info.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, innovation36T and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Applicant Primary Info", {
+// 	refresh(frm) {
+
+// 	},
+// });
