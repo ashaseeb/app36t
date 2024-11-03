6663f067cf28389ee7389e71875c45cc1baa1f51
Haseeb <ashaseeb@gmail.com>
2024-11-03 21:37:44 +0000

    complis screen imported

---
+++ app36t/applicant36t/doctype/complis_site_copied/complis_site_copied.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, innovation36T and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Complis Site Copied", {
+// 	refresh(frm) {
+
+// 	},
+// });
