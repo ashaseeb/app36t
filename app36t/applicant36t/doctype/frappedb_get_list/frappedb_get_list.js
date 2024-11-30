f0e3bda42fadd79e9eed50b1b02abc7d55e756be
Haseeb Ahmed <ashaseeb@gmail.com>
2024-11-30 01:09:06 +0000

    frappe.db work

---
+++ app36t/applicant36t/doctype/frappedb_get_list/frappedb_get_list.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, innovation36T and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("frappedb-get_list", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ app36t/applicant36t/doctype/frappedb_get_list/test_frappedb_get_list.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, innovation36T and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testfrappedbget_list(FrappeTestCase):
+	pass
